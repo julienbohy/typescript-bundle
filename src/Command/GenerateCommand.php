066ed95d644b47d4acc2736f7adbee9b74a4adc7
Julien Bohy <julien.bohy@fastback.be>
2026-01-12 13:00:52 +0100

    Add TypeScript reserved words

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -6,14 +6,16 @@ namespace JulienBohy\TypescriptBundle\Command;
 
 use Symfony\Component\Console\Attribute\Argument;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\Option;
 
 #[AsCommand(name: 'app:typescript:generate', description: 'Generate TypeScript definitions from PHP classes')]
 class GenerateCommand
 {
-
     public function __invoke(
-        #[Argument(description: 'Path to the directory containing the classes', name: 'path')]
-        $path
+        #[Option(description: 'Path to the directory containing the classes', name: 'path', shortcut: 'p')]
+        ?string $path = null,
+        #[Option(description: 'Path to the directory where the generated files will be saved', name: 'output-path', shortcut: 'o')]
+        ?string $outputPath = null
     )
     {
 
---
+++ src/Util/TypeScript.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace JulienBohy\TypescriptBundle\Util;
+
+use function Symfony\Component\String\u;
+
+class TypeScript
+{
+    public const RESERVED_KEYWORDS = [
+        'break',
+        'case',
+        'catch',
+        'class',
+        'const',
+        'continue',
+        'debugger',
+        'default',
+        'delete',
+        'do',
+        'else',
+        'export',
+        'extends',
+        'false',
+        'finally',
+        'for',
+        'function',
+        'if',
+        'import',
+        'in',
+        'instanceof',
+        'new',
+        'null',
+        'return',
+        'super',
+        'switch',
+        'this',
+        'throw',
+        'true',
+        'try',
+        'typeof',
+        'var',
+        'void',
+        'while',
+        'with',
+    ];
+
+}
